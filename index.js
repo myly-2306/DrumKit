79550482b63f3ac526380455022eac93a7353f9b
My Ly <ly.thuongmy71@gmail.com>
2022-10-01 17:10:53 -0700

    differentiate click and keyboard eventlistener

--- index.js
+++ index.js
@@ -1,50 +1,11 @@
+// clicking detection
 var buttonLength = document.querySelectorAll(".drum");
 
 for (var i = 0; i <= buttonLength.length; i++) {
 
     buttonLength[i].addEventListener("click", function (){
         this.style.color = "white";
-        var audio 
-        switch (this.innerHTML) {
-            case "w":
-                audio = new Audio("sounds/crash.mp3");
-                    audio.play();
-                break;
-            
-            case "a":
-                audio = new Audio("sounds/kick-bass.mp3");
-                    audio.play();
-                break;
-
-            case "s":
-                audio = new Audio("sounds/snare.mp3");
-                    audio.play();
-                break;
-
-            case "d":
-                audio = new Audio("sounds/tom-1.mp3");
-                    audio.play();
-                break;
-
-            case "j":
-                audio = new Audio("sounds/tom-2.mp3");
-                    audio.play();
-                break;
-
-            case "k":
-                audio = new Audio("sounds/tom-3.mp3");
-                    audio.play();
-                break;
-
-            case "l":
-                audio = new Audio("sounds/tom-4.mp3");
-                    audio.play();
-                break;
-        
-            default:
-                console.log("No button detected")
-                break;
-        }
+        makeSound(this.innerHTML);
     });
 };
 
@@ -54,8 +15,6 @@ for (var i = 0; i <= buttonLength.length; i++) {
 // fucntion to handle clicking the button
 
 
-// var audio = new Audio("sounds/tom-1.mp3");
-//         audio.play();
 
 
 // function HouseKeeper ( name, age, hasWorkPermit, language) {
@@ -63,7 +22,67 @@ for (var i = 0; i <= buttonLength.length; i++) {
 //     this.age = age;
 //     this.hasWorkPermit = hasWorkPermit;
 //     this.language = language;
+//     clean ();
 // }
 
 
-// var houseKeeper1 = new HouseKeeper ("Julie", 25, true, ["English,Spanish"]);
+// var houseKeeper1 = new HouseKeeper ("Julie", 25, true, ["English,Spanish"], clean());
+
+// function clean () {
+//     alert ("cleaning in process");
+// };
+
+
+
+// keydown detection
+document.addEventListener("keydown", function(event) {
+    console.log("this is pressed:", event );
+    makeSound(event.key);
+    
+});
+
+
+// function that make sound
+function makeSound(key) {
+    var audio 
+    switch (key) {
+        case "w":
+            audio = new Audio("sounds/crash.mp3");
+                audio.play();
+            break;
+        
+        case "a":
+            audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+            break;
+
+        case "s":
+            audio = new Audio("sounds/snare.mp3");
+                audio.play();
+            break;
+
+        case "d":
+            audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+            break;
+
+        case "j":
+            audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+            break;
+
+        case "k":
+            audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+            break;
+
+        case "l":
+            audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+            break;
+    
+        default:
+            console.log("No button detected")
+            break;
+    }
+};
